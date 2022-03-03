dd45709e55dfedd605db7c94af4ae0f3cebce1f1
Sharujan <sharus8@yahoo.fr>
2022-03-04 00:29:26 +0100

    e gen seems working ( else to be modified )

--- src/elang_gen.ml
+++ src/elang_gen.ml
@@ -44,9 +44,11 @@ let binop_of_tag =
 let rec make_eexpr_of_ast (a: tree) : expr res =
   let res =
     match a with
-    | Node(t, [e1; e2]) when tag_is_binop t ->
-         Error (Printf.sprintf "Unacceptable ast in make_eexpr_of_ast %s"
-                        (string_of_ast a))
+    | Node(t, [e1; e2]) when tag_is_binop t -> make_eexpr_of_ast e1 >>= fun e1res -> make_eexpr_of_ast e2 >>= fun e2res ->
+         OK(Ebinop(binop_of_tag t,e1res ,e2res))
+    | Node (Tneg,[e1]) -> make_eexpr_of_ast e1 >>= fun e1res ->  OK(Eunop(Eneg, e1res))
+    | StringLeaf(a) -> OK(Evar(a))
+    | IntLeaf(a) -> OK(Eint(a))
     | _ -> Error (Printf.sprintf "Unacceptable ast in make_eexpr_of_ast %s"
                     (string_of_ast a))
   in
@@ -54,10 +56,20 @@ let rec make_eexpr_of_ast (a: tree) : expr res =
     OK o -> res
   | Error msg -> Error (Format.sprintf "In make_eexpr_of_ast %s:\n%s"
                           (string_of_ast a) msg)
-
 let rec make_einstr_of_ast (a: tree) : instr res =
   let res =
     match a with
+    |Node(Tassign, [e1]) -> make_einstr_of_ast e1
+    |Node(Tassignvar,StringLeaf(a)::exp::[]) -> make_eexpr_of_ast exp >>= fun expres -> OK(Iassign(a,expres))
+    |Node(Twhile, [e1;e2]) -> make_eexpr_of_ast e1 >>= fun e1res -> make_einstr_of_ast e2 >>= fun e2res -> OK(Iwhile(e1res,e2res))
+    |Node(Tblock,linstr) ->let get_instr x = match (make_einstr_of_ast x) with |OK v -> v |_ -> Ireturn(Evar("ERROR"))
+                           in  OK(Iblock(List.map get_instr linstr))
+    |Node(Treturn, [e]) ->make_eexpr_of_ast e >>= fun eres -> OK(Ireturn(eres))
+    |Node(Tprint, [e]) ->make_eexpr_of_ast e >>= fun eres -> OK(Iprint(eres))
+    |Node(Tif, cond::prem::[]) -> make_eexpr_of_ast cond >>= fun condres -> make_einstr_of_ast prem >>= fun premres -> OK(Iif(condres,premres,Iprint(Evar("No else"))))
+    |Node(Tif, cond::prem::sec::[]) ->make_eexpr_of_ast cond >>= fun condres -> make_einstr_of_ast prem >>= fun premres -> make_einstr_of_ast sec >>= fun secres -> OK(Iif(condres, premres, secres))
+
+
     | _ -> Error (Printf.sprintf "Unacceptable ast in make_einstr_of_ast %s"
                     (string_of_ast a))
   in
@@ -76,9 +88,7 @@ let make_ident (a: tree) : string res =
 let make_fundef_of_ast (a: tree) : (string * efun) res =
   match a with
   | Node (Tfundef, [StringLeaf fname; Node (Tfunargs, fargs); fbody]) ->
-    list_map_res make_ident fargs >>= fun fargs ->
-     (* TODO *)
-     Error "make_fundef_of_ast: Not implemented, yet."
+                  list_map_res make_ident fargs >>= fun fargs ->make_einstr_of_ast fbody >>= fun fbodyres -> OK(fname, {funargs = fargs; funbody = fbodyres })
   | _ ->
     Error (Printf.sprintf "make_fundef_of_ast: Expected a Tfundef, got %s."
              (string_of_ast a))
